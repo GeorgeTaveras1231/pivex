906a452b8d5b86781d3d8f857e0bf2b4e95614ea
George Taveras <gtaveras@xogrp.com>
2015-06-13 21:00:54 -0400

    [chore] Replace stories_path tests for doctest,
    also include simple doctest for Pivex.Stories.get

--- lib/pivex/stories.ex
+++ lib/pivex/stories.ex
@@ -1,13 +1,23 @@
 defmodule Pivex.Stories do
-
   @url "https://www.pivotaltracker.com/services/v5"
   @client HTTPoison
+
+  @doc """
+    Make a request to stories_path
+  """
 
   def get(token, project_id) do
     stories_path(project_id) |>
       @client.get [{"X-TrackerToken", token}]
   end
 
+  @doc """
+    Get the stories path for a project
+
+    Examples:
+      iex> Pivex.Stories.stories_path("123")
+      "#{@url}/projects/123/stories"
+  """
   def stories_path(project_id) do
     "#{@url}/projects/#{project_id}/stories"
   end
--- test/stories_test.exs
+++ test/stories_test.exs
@@ -1,11 +1,9 @@
 defmodule StoriesTest do
   use ExUnit.Case, async: true
   import Mock
-  @api_url "https://www.pivotaltracker.com/services/v5"
+  doctest Pivex.Stories
 
-  test ".stories_path(project_id)" do
-    assert Pivex.Stories.stories_path("123") == @api_url <> "/projects/123/stories"
-  end
+  @api_url "https://www.pivotaltracker.com/services/v5"
 
   test_with_mock ".get(token, project_id)", HTTPoison,
     [get: fn (_url, _headers) -> "[]" end] do
